9b05207da745c08f1c2e58727d10e98e12d4fbf0
ryo33 <ryo12redstone@gmail.com>
2015-08-20 11:02:50 +0900

    Modify PrototypeUI

--- modui_components/PrototypeUI.php
+++ modui_components/PrototypeUI.php
@@ -3,31 +3,32 @@
 class PrototypeUI extends ModUIComponent{
 
     public $_con;
-    private $args;
 
     public function __construct($con, $args){
         $this->_con = $con;
-        $this->args = $args;
+        foreach($args as $key => $arg){
+            $this->$key = $arg;
+        }
     }
 
     public function get_template_name($name){
-        return (isset($this->args['template_name']))? $this->args['template_name']($this, $name): $name;
+        return (isset($this->template_name))? $this->template_name($this, $name): $name;
     }
 
     public function get_templates($name){
-        return $this->args['templates']($this, $name);
+        return $this->templates($this, $name);
     }
 
     public function get_values($name){
-        return (isset($this->args['values']))? $this->args['values']($this, $name): [];
+        return (isset($this->values))? $this->values($this, $name): [];
     }
 
     public function get_scripts($name){
-        return (isset($this->args['scripts']))? $this->args['scripts']($this, $name): [];
+        return (isset($this->scripts))? $this->scripts($this, $name): [];
     }
 
     public function input($name, $value){
-        if(isset($this->args['input'])) $this->args['input']($this, $name, $value);
+        if(isset($this->input)) $this->input($this, $name, $value);
     }
 
 }

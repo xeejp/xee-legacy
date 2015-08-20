d492af5d376e3307ceb6bc9d7576c5dd9c79bd33
ryo33 <ryo12redstone@gmail.com>
2015-08-20 11:58:18 +0900

    Add get/set_personal to Controller

--- require/Controller.php
+++ require/Controller.php
@@ -9,6 +9,14 @@ class Controller{
         $this->modui = $modui;
     }
 
+    public function get_personal($name, $id=null, $default=null){
+        $this->get('_participant_' . ($id != null ? $id : $this->participant['id']) . '_' . $name, $default);
+    }
+
+    public function set_personal($name, $value, $id=null){
+        $this->get('_participant_' . ($id != null ? $id : $this->participant['id']) . '_' . $name, $value);
+    }
+
     public function get($name, $default=null){
         return $this->vdb->get($name, $default);
     }

25ef1e88f5035e524159baff6884b7343ff6a55c
InsideOfBamboo <tt0919@xee.jp>
2015-08-20 11:11:55 +0900

    Fix the bug

--- modui_components/PrototypeUI.php
+++ modui_components/PrototypeUI.php
@@ -12,23 +12,23 @@ class PrototypeUI extends ModUIComponent{
     }
 
     public function get_template_name($name){
-        return (isset($this->template_name))? $this->template_name($this, $name): $name;
+        return (isset($this->template_name))? call_user_func($this->template_name, $this, $name): $name;
     }
 
     public function get_templates($name){
-        return $this->templates($this, $name);
+        return call_user_func($this->templates, $this, $name);
     }
 
     public function get_values($name){
-        return (isset($this->values))? $this->values($this, $name): [];
+        return (isset($this->values))? call_user_func($this->values, $this, $name): [];
     }
 
     public function get_scripts($name){
-        return (isset($this->scripts))? $this->scripts($this, $name): [];
+        return (isset($this->scripts))? call_user_func($this->scripts, $this, $name): [];
     }
 
     public function input($name, $value){
-        if(isset($this->input)) $this->input($this, $name, $value);
+        if(isset($this->input)) call_user_func($this->input, $this, $name, $value);
     }
 
 }

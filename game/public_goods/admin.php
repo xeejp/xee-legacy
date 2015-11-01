79bd20cf7ad0606115f965e11d359280e6886cba
kage-mac <kagemiku@gmail.com>
2015-11-01 10:12:04 +0900

    modify admin.php in order to implement Public Goods Experiment.

--- game/public_goods/admin.php
+++ game/public_goods/admin.php
@@ -4,8 +4,6 @@ $container = new NormalContainer();
 // options
 $container->add(new StaticUI('[設定]<br/>'));
 $container->add(new StaticUI('ExpID : '. $_con->experiment['password'] .'<br/>'));
-$container->add(new StaticUI('損失を許可する : '));
-$container->add(new OptionUI($_con, 'allow_loss', $_con->get('allow_loss')));
 
 // participants
 $container->add(new ParticipantsList($_con));
@@ -13,54 +11,62 @@ $container->add(new ParticipantsManagement($_con));
 
 $container->add($modulator = new PageContainer($_con->get('status', 'wait')));
 $modulator->add_page('wait', new MatchingButton($_con,
-    function($con){
+    function($con) {
         $num = 0;
         foreach ($con->participants as $participant) {
             if ($con->get_personal('active', false, $participant['id']))
                 $num++;
         }
-        return ($num>0) && ($num%2 == 0);
+        return ($num == 4);
     },
-    function($con){
-        $count = 1;
+    function($con) {
         $result = [];
-        foreach($con->participants as $participant){
-            if (!$con->get_personal('active', false, $participant['id'])) continue;
-            $con->set_personal('role', ($count % 2 == 1)? 'seller': 'buyer', $participant['id']);
-            if ($con->get_personal('role', null, $participant['id']) == 'seller') {
-                $con->set_personal('money', 0, $participant['id']);
-                $con->set_personal('cost', $count * 100, $participant['id']);
-            } else {
-                $con->set_personal('money', $count * 100, $participant['id']);
-                $con->set_personal('cost', 0, $participant['id']);
+        foreach($con->participants as $participant) {
+            if (!$con->get_personal('active', false, $participant['id'])) {
+                continue;
             }
-            $con->set_personal('price', 0, $participant['id']);
-            $count++;
+
+            $con->set_personal('cur_pt', 20, $participant['id']);
+            $con->set_personal('sum_pt', 0, $participant['id']);
+            $con->set_personal('invest_pt', 0, $participant['id']);
+            $con->set_personal('punish_pt', 0, $participant['id']);
+            $con->set_personal('punish_id', 0, $participant['id']);
         }
         $con->set('status', 'ready');
+
         return $result;
     }
 ));
+
 $modulator->add_page('ready', $_ready = new NormalContainer());
 $_ready->add(new ButtonUI($_con,
-    function($_con){ return "再マッチング"; },
-    function($_con){ $_con->set('status', 'wait'); }
+    function($_con) {
+        return "再マッチング"; 
+    },
+    function($_con) { 
+        $_con->set('status', 'wait');
+    }
 ));
 $_ready->add(new ButtonUI($_con,
-    function($_con){ return "開始"; },
-    function($_con){
+    function($_con) {
+        return "開始"; 
+    },
+    function($_con) {
         $_con->set('status', 'experiment');
         foreach ($_con->participants as $participant) {
-            if ($_con->get_personal('active', false, $participant['id']))
+            if ($_con->get_personal('active', false, $participant['id'])) {
                 $_con->set_personal('page', 'experiment', $participant['id']);
-            else
+            } else {
                 $_con->set_personal('page', 'reject', $participant['id']);
+            }
         }
     }
 ));
 $modulator->add_page('experiment', new ButtonUI($_con,
-    function($_con){ return 'リセット'; },
-    function($_con){
+    function($_con) {
+        return 'リセット'; 
+    },
+    function($_con) {
         $_con->set('status', 'wait');
         foreach ($_con->participants as $participant) {
             $_con->set_personal('page', 'reject', $participant['id']);
